0743d2c2b936bccfa2972ba3ac0cf0b4a43c6f22
Hércules <116934166+thellllima02@users.noreply.github.com>
2025-01-31 15:28:47 -0300

    .

--- FRONTEND-MOBILE/loja-ifbaiano/src/screens/Home/Home.tsx
+++ FRONTEND-MOBILE/loja-ifbaiano/src/screens/Home/Home.tsx
@@ -1,27 +1,67 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 24, fontWeight: "bold" }}>Tela Inicial</Text>
-      <Button 
-        title="Adicionar Clientes" 
-        onPress={() => navigation.navigate("Cadastro Cliente")} 
-      />
-      <Button 
-        title="Cadastrar Produtos" 
-        onPress={() => navigation.navigate("Cadastro Produto")} 
-      />
-      <Button 
-        title="Cadastrar Vendas" 
-        onPress={() => navigation.navigate("Cadastro Venda")} 
-      />
+    <View style={styles.container}>
+      <Text style={styles.title}>Tela Inicial</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity 
+          style={styles.button} 
+          onPress={() => navigation.navigate("Cadastro Cliente")}
+        >
+          <Text style={styles.buttonText}>Adicionar Clientes</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+          style={styles.button} 
+          onPress={() => navigation.navigate("Cadastro Produto")}
+        >
+          <Text style={styles.buttonText}>Cadastrar Produtos</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+          style={styles.button} 
+          onPress={() => navigation.navigate("Cadastro Venda")}
+        >
+          <Text style={styles.buttonText}>Cadastrar Vendas</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#f5f5f5", // Cor de fundo
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 30,
+    color: "#333", // Cor do título
+  },
+  buttonContainer: {
+    width: "100%",
+    marginTop: 10,
+  },
+  button: {
+    backgroundColor: "#773189",
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    marginVertical: 10,
+    borderRadius: 15,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
 export default HomeScreen;
-
-
-
